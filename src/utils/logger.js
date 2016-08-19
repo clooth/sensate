010d59b82b2eb8394a1b8d106db4eac8dea6c69e
Nico Hämäläinen <nico@sizeof.io>
2016-08-20 02:38:20 +0300

    refactor(logger): Replace tracer with winston

---
+++ src/utils/logger.js
@@ -0,0 +1,23 @@
+/** @module utils/logger */
+import Winston from 'winston'
+
+let logger = null
+
+if (process.env.NODE_ENV !== 'test') {
+  logger = new Winston.Logger({
+    transports: [
+      new Winston.transports.Console({
+        level: process.env.LOG_LEVEL || 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+      })
+    ]
+  })
+} else {
+  logger = new Winston.Logger({
+    transports: []
+  })
+}
+
+export default logger
